4f823826fe1ab51989add59439d28ee37f930e7f
konteh <kontehebrima993@gmail.com>
2026-02-09 16:18:57 +0000

    image title

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -80,7 +80,7 @@ export function Gallery() {
                   className="w-full h-auto object-cover group-hover:scale-110 transition-transform duration-500"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end">
-                  <p className="text-white p-4">{image.title}</p>
+                  {/* <p className="text-white p-4">{image.title}</p> */}
                 </div>
               </div>
             ))}
